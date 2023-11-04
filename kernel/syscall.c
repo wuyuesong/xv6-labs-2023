debcd7b130b473508c7a223825f655a05fbabe03
wuyuesong <1377030423@qq.com>
2023-11-04 15:14:40 +0800

    prepare for sysinfo

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -134,8 +134,7 @@ syscall(void)
   int num;
   struct proc *p = myproc();
 
-//   num = p->trapframe->a7;
-  num = * (int *) 0;
+  num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
