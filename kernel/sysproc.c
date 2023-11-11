c6b7995b775c76972098537749edd0e476050758
wuyuesong <1377030423@qq.com>
2023-11-11 22:52:57 +0800

    lab3 finished

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -75,6 +75,27 @@ int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 page_addr, abits;
+  pte_t* pte;
+  unsigned int bit_mask = 0;
+  int page_num;
+  pagetable_t pagetable;
+  argaddr(0, &page_addr);
+  argint(1, &page_num);
+  argaddr(2, &abits);
+  if (page_num > 32) {
+    return -1;
+  }
+  pagetable = myproc()->pagetable;
+
+  for (int i = 0; i < page_num; i ++) {
+    pte = walk(pagetable, page_addr + i * PGSIZE, 0);
+    if (*pte & PTE_A) {
+        bit_mask |= (1L << i);
+        *pte &= ~(PTE_A);
+    }
+  }
+  copyout(pagetable, abits, (char *)&bit_mask, 4);
   return 0;
 }
 #endif
