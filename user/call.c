7792cb45641e2cc9fa8717a034a5bc246187eae9
wuyuesong <1377030423@qq.com>
2023-11-12 22:47:01 +0800

    test2

--- user/call.c
+++ user/call.c
@@ -14,6 +14,6 @@ int f(int x) {
 void main(void) {
   printf("%d %d\n", f(8)+1, 13);
   unsigned int i = 0x00646c72;
-  printf("H%x Wo%s\n", 57616, &i);
+  printf("H%x Wo%s", 57616, &i);
   exit(0);
 }
