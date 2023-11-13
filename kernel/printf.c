1d200ceb492428ed8695ec650b38f3274b943507
wuyuesong <1377030423@qq.com>
2023-11-13 17:20:47 +0800

    lab4 backtrace finished

--- kernel/printf.c
+++ kernel/printf.c
@@ -133,3 +133,17 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace() 
+{   
+    printf("backtrace:\n");
+    uint64 fp = r_fp();
+    uint64 page_low = PGROUNDDOWN(fp);
+    while (fp >= page_low)
+    {
+        // printf("fp: %p\n", fp);
+        uint64 ra =  *(uint64 *) (fp - 8);
+        fp = *(uint64 *) (fp - 16);
+        printf("%p\n", ra);
+    }
+}
